6d4f6735295ffb126dc887299251cdd4629fd0a2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-30 09:41:20 -0300

    routes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const rotaProdutos = require('./routes/produtos');
+
+
+app.use('/produtos', rotaProdutos);
 app.use((req, res, next) =>{
     res.status(200).send({
         mensagem: 'OK, deu certo'
