ca5295a6f19f008416bcfcd65f3baa77e7e36172
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-30 09:52:40 -0300

    Var id

--- routes/produtos.js
+++ routes/produtos.js
@@ -13,4 +13,21 @@ router.post('/',(req, res, next) =>{
     });
 }); 
 
+router.get('/:id_produto', (req, res, next) =>{
+    const id = req.params.id_produto;
+
+    if(id === 1){
+        res.status(200).send({
+            mensagem: 'Id especial',
+            id: id
+        });
+    }else{
+        res.status(200).send({
+            mensagem: 'Usando GET da nova rota',
+            id: id
+        });
+    }
+    
+});
+
 module.exports = router;
