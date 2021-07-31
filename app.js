e61e3c4158f79a552c7e95de19774a47d8365908
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-31 08:52:49 -0300

    body Parser

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('bodyParser');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
