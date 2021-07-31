af08a428d9ccac4820963ad07109577aa5d0e26b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-31 09:14:36 -0300

    CORS update
    Controle de permissões de origin, header, request, content-type, authorization e methods

--- app.js
+++ app.js
@@ -10,10 +10,23 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));  //apenas dados simples
 app.use(bodyParser.json());  //apenas formato json
 
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+
+    next();
+});
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
-
-
 
 // Tratando Erros
 app.use((req, res, next) =>{
