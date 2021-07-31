b1ebd43ba86e8af39ba91466075c8957979c8bc3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-31 08:38:56 -0300

    morgan

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use(morgan('dev'));
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
