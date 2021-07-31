864e7636f38472e46a5837e3fc8117d5179bc49b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-31 08:48:30 -0300

    Tratamento de erros

--- app.js
+++ app.js
@@ -10,5 +10,20 @@ app.use(morgan('dev'));
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+app.use((req, res, next) =>{
+    const erro = new Error('Não encontrado');
+    erro.status =404;
+    next(erro);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    return res.send({
+        erro:{
+            mensagem: error.message
+        }
+    });
+});
+
 
 module.exports = app;
